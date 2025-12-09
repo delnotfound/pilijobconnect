cfdc92a61c05e3c202a61f9d23ed9912fccaad1c
greyratpaul34-creator <greyratpaul34@gmail.com>
2025-12-09 22:06:52 +0800

    Ready for redefense

--- client/src/pages/AdminDashboard.tsx
+++ client/src/pages/AdminDashboard.tsx
@@ -818,7 +818,7 @@ export function AdminDashboard() {
             {/* Most In-Demand Skills */}
             <Card>
               <CardHeader>
-                <CardTitle>Mamamo vs Supply Analysis</CardTitle>
+                <CardTitle>Skills Demand vs Supply Analysis</CardTitle>
                 <CardDescription>
                   Compares skills demanded by employers vs skills available from
                   job seekers. Positive gap means shortage, negative means
